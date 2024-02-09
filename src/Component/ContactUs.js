92248b9208d6d7bc623247372f5fb776391616fd
Karmanya <karmanyasingh8@gmail.com>
2024-02-10 02:37:10 +0530

    contact us improved

--- src/Component/ContactUs.js
+++ src/Component/ContactUs.js
@@ -1,5 +1,3 @@
-// ContactUs.jsx
-
 import React from 'react';
 
 function ContactUs() {
@@ -21,15 +19,18 @@ function ContactUs() {
                                             <input
                                                 type="text"
                                                 className="form-control form-group"
+                                                id="name"
                                                 placeholder="Name"
                                             />
                                             <input
-                                                type="text"
+                                                type="email"
                                                 className="form-control form-group"
+                                                id="email"
                                                 placeholder="Email"
                                             />
                                             <textarea
                                                 className="form-control form-group"
+                                                id="message"
                                                 placeholder="Message"
                                             ></textarea>
                                             <button className="contact_form_submit">Send</button>
@@ -38,69 +39,57 @@ function ContactUs() {
                                 </div>
                                 <div className="col-md-2">
                                     <div className="right_conatct_social_icon d-flex align-items-end">
-                                        <div className="socil_item_inner d-flex">
+                                        <ul className="socil_item_inner d-flex">
                                             <li>
-                                                <a href="#">
-                                                    <i className="fab fa-facebook-square"></i>
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <a href="https://www.instagram.com/glitch.shoolini/">
+                                                <a href="https://www.instagram.com/glitch.shoolini/" target="_blank" rel="noopener noreferrer">
                                                     <i className="fab fa-instagram"></i>
                                                 </a>
                                             </li>
                                             <li>
-                                                <a href="https://twitter.com/glitchshoolini">
+                                                <a href="https://twitter.com/glitchshoolini" target="_blank" rel="noopener noreferrer">
                                                     <i className="fab fa-twitter"></i>
                                                 </a>
                                             </li>
                                             <li>
-                                                <a href="#">
+                                                <a href="https://discord.gg/s5fDEaXFjx" target="_blank" rel="noopener noreferrer">
                                                     <i className="fab fa-discord"></i>
                                                 </a>
                                             </li>
-
-                                        </div>
+                                        </ul>
                                     </div>
                                 </div>
                             </div>
-                            <div className="contact_info_sec"></div>
-
-
                         </div>
                     </div>
                 </div>
             </div>
 
             <section className="map_sec">
-    <div className="container">
-        <div className="row">
-            <div className="col-md-10 offset-md-1">
-                <div className="map_inner">
-                    <h4>Find Us on Google Map</h4>
-                    <div className="map_bind">
-                        <iframe
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3424.796933768103!2d77.11584387558557!3d30.864359874521046!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390f839442ab0b5b%3A0x6d5230f107c2117a!2sShoolini%20University!5e0!3m2!1sen!2sin!4v1707056481078!5m2!1sen!2sin"
-                            width="100%"
-                            height="350"
-                            frameBorder="0"
-                            title="Google Maps Of Shoolini"
-                            style={{ border: '0' }}
-                            allowFullScreen
-                            aria-hidden="false"
-                            tabIndex="0"
-                        ></iframe>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-10 offset-md-1">
+                            <div className="map_inner">
+                                <h4>Find Us on Google Map</h4>
+                                <div className="map_bind">
+                                    <iframe
+                                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3424.796933768103!2d77.11584387558557!3d30.864359874521046!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390f839442ab0b5b%3A0x6d5230f107c2117a!2sShoolini%20University!5e0!3m2!1sen!2sin!4v1707056481078!5m2!1sen!2sin"
+                                        width="100%"
+                                        height="350"
+                                        frameBorder="0"
+                                        title="Google Maps Of Shoolini"
+                                        style={{ border: '0' }}
+                                        allowFullScreen
+                                        aria-hidden="false"
+                                        tabIndex="0"
+                                    ></iframe>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
-    </div>
-</section>
-
-
-
+            </section>
         </>
     );
-};
+}
 
 export default ContactUs;
