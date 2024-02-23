d87c10950f11847c2ab26b0320580d2a217dab41
Karmanya <karmanyasingh8@gmail.com>
2024-02-23 23:49:09 +0530

    buttons placed

--- src/Component/Events.js
+++ src/Component/Events.js
@@ -9,37 +9,39 @@ function Events() {
             <div className="event-page-container">
                 <div className="event-cards">
                     <Card className="event-card">
+                        <Card.Img variant="top" alt="poster" src={require("../Assets/ctf.png")} />
                         <Card.Body>
-                            <Card.Img variant="top" alt="poster" src={require("../Assets/ctf.png")} />
-                        </Card.Body>
-                        <Button variant="primary" href="registration_ctf">Register</Button>
-                    </Card>
-                    <Card className="event-card">
-                        <div className="card-overlay">
-                            <Card.Img variant="top" alt="poster" src={require("../Assets/hackathon.png")} />
-                            <div className="overlay-text">This</div>
-                        </div>
-                        <Card.Body>
+                            <div className="text">This is CTF</div>
                             <Button variant="primary" href="registration_ctf">Register</Button>
                         </Card.Body>
                     </Card>
                     <Card className="event-card">
+                        <Card.Img variant="top" alt="poster" src={require("../Assets/hackathon.png")} />
                         <Card.Body>
-                            <Card.Img variant="top" alt="e-sports" src={require("../Assets/gaming.png")} />
+                            <div className="text">This Is Hackathon</div>
+                            <Button variant="primary" href="registration_hackathon">Register</Button>
                         </Card.Body>
-                        <Button variant="primary" href="https://shorturl.at/DRSW3">Register</Button>
                     </Card>
                     <Card className="event-card">
+                        <Card.Img variant="top" alt="e-sports" src={require("../Assets/gaming.png")} />
                         <Card.Body>
-                            <Card.Img variant="top" alt="poster" src={require("../Assets/mmm.jpg")} />
+                            <div className="text">This is E-Sports</div>
+                            <Button variant="primary" href="https://shorturl.at/DRSW3">Register</Button>
                         </Card.Body>
-                        <Button variant="primary" href="registration_micromousemaze">Register</Button>
                     </Card>
                     <Card className="event-card">
+                        <Card.Img variant="top" alt="poster" src={require("../Assets/mmm.png")} />
                         <Card.Body>
-                            <Card.Img variant="top" alt="poster" src={require("../Assets/robo.png")} />
+                            <div className="text">This is Micro Mouse Maze</div>
+                            <Button variant="primary" href="registration_micromousemaze">Register</Button>
                         </Card.Body>
-                        <Button variant="primary" href="registration_robowar">Register</Button>
+                    </Card>
+                    <Card className="event-card">
+                        <Card.Img variant="top" alt="poster" src={require("../Assets/robo.png")} />
+                        <Card.Body>
+                            <div className="text">This is Robowars</div>
+                            <Button variant="primary" href="registration_robowar">Register</Button>
+                        </Card.Body>
                     </Card>
                 </div>
             </div>
